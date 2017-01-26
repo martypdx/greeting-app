3253337ea8ae071f79f159bfef58e8fa53e40a4e
martypdx <marty@martypdx.com>
2017-01-26 14:06:42 -0800

    extract getOptions to function

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,14 +1,43 @@
-var assert = require('assert');
-var run = require('./run.js');
+const assert = require('assert');
+const run = require('./run.js');
+const cowsay = require('cowsay');
 
 describe('greeting app', function() {
-    it('greets by name', function() {
-        var output = run(['./lib/greet.js', 'marty']);
-        assert.equal(output, 'hello marty\n');
+
+    describe('plain mode', () => {
+
+        it('<name> --plain', function() {
+            const output = run(['./lib/greet.js', 'marty', '--plain']);
+            assert.equal(output, 'hello marty\n');
+        });    
+        
+        it('--plain <name>', function() {
+            const output = run(['./lib/greet.js', 'marty', '--plain']);
+            assert.equal(output, 'hello marty\n');
+        });
+
+        it('"stranger" when no name ', function() {
+            const output = run(['./lib/greet.js', '--plain']);
+            assert.equal(output, 'hello stranger\n');
+        });
     });
 
-    it('uses "stranger" as a default when no name provided', function() {
-        var output = run(['./lib/greet.js']);
-        assert.equal(output, 'hello stranger\n');
+    describe('cowsay mode', () => {
+
+        it('<name>', function() {
+            const output = run(['./lib/greet.js', 'marty']);
+            const expected = cowsay.say({
+                text : 'hello marty'
+            });
+            assert.equal(output, expected + '\n');
+        });    
+        
+        it('"stranger" when no name ', function() {
+            const output = run(['./lib/greet.js']);
+            const expected = cowsay.say({
+                text : 'hello stranger'
+            });
+            assert.equal(output, expected + '\n');
+        });
     });
 });
