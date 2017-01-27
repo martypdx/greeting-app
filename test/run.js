21d0f22268d92b8705929f205d6828842f938a7d
martypdx <marty@martypdx.com>
2017-01-26 16:18:47 -0800

    delete obsolete file!!!!

--- test/run.js
+++
@@ -1,9 +0,0 @@
-var child_process = require('child_process');
-
-module.exports = function run(args) {
-    return child_process.execFileSync(
-        'node',
-        args,
-        { encoding: 'utf8' }
-    );
-};
