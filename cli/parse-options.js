a619a174d6a03515f20c72a1a94931852be87a70
martypdx <marty@martypdx.com>
2017-01-26 16:12:09 -0800

    improve variable for option key

--- cli/parse-options.js
+++ cli/parse-options.js
@@ -2,9 +2,9 @@ module.exports = function getOptions(args) {
 
     const options = args.reduce((options, a) => {
         const split = a.split(':');
-        const name = split[0].replace('--', ''); // --name
+        const key = split[0].replace('--', ''); // --name
         const value = split[1]; // martha
-        options[name] = value;
+        options[key] = value;
         return options;
     }, {});
 
